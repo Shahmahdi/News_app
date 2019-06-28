5eeb48eeb6f2c50c7485c2048a990faf94040255
Mahdi <shahmahdi11@gmail.com>
2019-06-28 23:34:31 +0600

    search option added

--- src/NewsApp.tsx
+++ src/NewsApp.tsx
@@ -8,15 +8,26 @@ interface NewsProps {
 export const NewsApp = () => {
 
   const [news, setNews] = useState([] as NewsProps[]);
+  const [searchQuery, setSearchQuery] = useState('react')
 
   useEffect(() => {
-    axios.get('https://hn.algolia.com/api/v1/search?query=react')
+    axios.get(`https://hn.algolia.com/api/v1/search?query=${searchQuery}`)
     .then((res: any) => { setNews(res.data.hits); })
     .catch((err: any) => console.log(err));
-  }, [news]);
+  }, [searchQuery]);
 
   return (
     <div>
+      <form>
+        <h4>Search text: </h4>
+        <input 
+          type="text"
+          value={searchQuery}
+          onChange={e => {
+            setSearchQuery(e.target.value);
+          }}
+        />
+      </form>
       <h2>React News:</h2>
       <ul>
         {news.map((n) => (
